6c561f69c283ea773d0af7e3fec0cb2ca2e0986c
sys910070 <sys910070@gmail.com>
2022-05-17 17:20:26 +0800

    0
    test

---
+++ ap.py
@@ -0,0 +1,11 @@
+class ap:
+    def __init__(self, x, y, power, channel, capacity, id, type_ap):
+        self.x = x
+        self.y = y
+        self.power = power
+        self.channel = channel
+        self.capacity = capacity
+        self.id = id
+        self.type = type_ap
+    
+    def check_state()
--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print('HELLO WORLD!')
+# import numpy as np
+# import random
+# import math
+# import matplotlib.pyplot as plt
+
+# class ap:
+#     def _init_(self, x, y, power, channel, capacity, id, type_ap):
+#         self.x = x
+#         self.y = y
+#         self.power = power
+#         self.channel = channel
+#         self.capacity = capacity
+#         self.id = id
+#         self.type = type_ap
+
+# class device:
+#     def _init_(self, x, y, power, channel, id, type_device, dis):
+#         self.x = x
+#         self.y = y
+#         self.power = power
+#         self.channel = channel
+#         self.id = id
+#         self.type = type_device
+#         self.dis = dis
+
+# power_level = [p_max, p_max/2, p_max/4, p_max/8, p_max/16, 0]
+# frequency_channel_20 = []
+# frequency_channel_40 = []
+# frequency_channel_80 = []
+# frequency_channel_160 = []
+
+# #distance between two device(AP or STA)
+# def distance(a, b):
+#     return ((a[0]-b[0])**2+(a[1]-b[1])**2)**(1/2)
+
+for i in range(10):
+    print(i)
